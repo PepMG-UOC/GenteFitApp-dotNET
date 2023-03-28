03bf9f6d107792ac2f04eb8c198c867267043ae6
PepMG-UOC <115872651+PepMG-UOC@users.noreply.github.com>
2023-03-28 16:20:29 +0200

    gestion centro

--- GenteFitApp/Conrolers/GestionCentro.cs
+++ GenteFitApp/Conrolers/GestionCentro.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GenteFitApp.Modelo;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,61 @@ using System.Threading.Tasks;
 
 namespace GenteFitApp.Conrolers
 {
-    internal class GestionCentro
+    public class GestionCentro
     {
+        public void altaActividad(string nombre, string descripcion, int monitorID)
+        {
+            using (GenteFitDBEntities dBGfit = new GenteFitDBEntities())
+            {
+                Actividad nuevaActividad = new Actividad
+                {
+                    nombre = nombre,
+                    descripcion = descripcion,
+                    monitorID = monitorID
+                };
+
+                dBGfit.Actividad.Add(nuevaActividad);
+                dBGfit.SaveChanges();
+            }
+        }
+
+        public void bajaActividad(int IDActividad)
+        {
+            using (GenteFitDBEntities dBGfit = new GenteFitDBEntities())
+            {
+                Actividad borrarActividad = dBGfit.Actividad.Find(IDActividad);
+
+                if (borrarActividad != null)
+                {
+                    dBGfit.Actividad.Remove(borrarActividad);
+                    dBGfit.SaveChanges();
+                }
+            }
+        }
+
+
+        // ---
+
+        public void altaSala(int numPlazas, int dimensionM2)
+        {
+            using (GenteFitDBEntities dBGfit = new GenteFitDBEntities())
+            {
+                Sala nuevaSala = new Sala
+                {
+                    numPlazas = numPlazas,
+                    dimensionM2 = dimensionM2
+                };
+                dBGfit.Sala.Add(nuevaSala);
+                dBGfit.SaveChanges();
+            }
+        }
+
+
+
+
+
+
+
+
     }
 }
