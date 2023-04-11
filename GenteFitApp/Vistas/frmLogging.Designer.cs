be09c94e6b17f69b4449e83f44e00d2cfbaa14e4
PepMG-UOC <115872651+PepMG-UOC@users.noreply.github.com>
2023-04-11 17:34:05 +0200

    Montando Vistas

---
+++ GenteFitApp/Vistas/frmLogging.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace GenteFitApp.Vistas
+{
+    partial class frmLogging
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmLogging));
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tbEmail = new System.Windows.Forms.TextBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.tbPassw = new System.Windows.Forms.TextBox();
+            this.btnAccede = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pictureBox2.Image = global::GenteFitApp.Properties.Resources.User_avatar_svg;
+            this.pictureBox2.Location = new System.Drawing.Point(460, 415);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(34, 33);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pictureBox1.Image = global::GenteFitApp.Properties.Resources.GenteFit_Entrena_y_vive;
+            this.pictureBox1.Location = new System.Drawing.Point(448, 57);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(301, 319);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // tbEmail
+            // 
+            this.tbEmail.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.tbEmail.Font = new System.Drawing.Font("Microsoft YaHei", 12F);
+            this.tbEmail.Location = new System.Drawing.Point(525, 419);
+            this.tbEmail.Name = "tbEmail";
+            this.tbEmail.Size = new System.Drawing.Size(203, 29);
+            this.tbEmail.TabIndex = 2;
+            this.tbEmail.Text = "eMail";
+            this.tbEmail.Enter += new System.EventHandler(this.tbEmail_Enter);
+            this.tbEmail.Leave += new System.EventHandler(this.tbEmail_Leave);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(460, 468);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(34, 36);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            // 
+            // tbPassw
+            // 
+            this.tbPassw.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.tbPassw.Font = new System.Drawing.Font("Microsoft YaHei", 12F);
+            this.tbPassw.Location = new System.Drawing.Point(525, 475);
+            this.tbPassw.Name = "tbPassw";
+            this.tbPassw.Size = new System.Drawing.Size(203, 29);
+            this.tbPassw.TabIndex = 4;
+            this.tbPassw.Text = "password";
+            this.tbPassw.Enter += new System.EventHandler(this.tbPassw_Enter);
+            this.tbPassw.Leave += new System.EventHandler(this.tbPassw_Leave);
+            // 
+            // btnAccede
+            // 
+            this.btnAccede.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnAccede.Font = new System.Drawing.Font("Microsoft YaHei", 12F);
+            this.btnAccede.Location = new System.Drawing.Point(541, 544);
+            this.btnAccede.Name = "btnAccede";
+            this.btnAccede.Size = new System.Drawing.Size(110, 40);
+            this.btnAccede.TabIndex = 5;
+            this.btnAccede.Text = "Accede";
+            this.btnAccede.UseVisualStyleBackColor = true;
+            this.btnAccede.Click += new System.EventHandler(this.btnAccede_Click);
+            // 
+            // frmLogging
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1267, 714);
+            this.Controls.Add(this.btnAccede);
+            this.Controls.Add(this.tbPassw);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.tbEmail);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmLogging";
+            this.Text = "frmLogging";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.frmLogging_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox tbEmail;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.TextBox tbPassw;
+        private System.Windows.Forms.Button btnAccede;
+    }
+}
---
+++ GenteFitApp/Vistas/frmMenuCliente.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace GenteFitApp.Vistas
+{
+    partial class frmMenuCliente
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.pbClases = new System.Windows.Forms.PictureBox();
+            this.pbMisClases = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbClases)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbMisClases)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnSalir.Font = new System.Drawing.Font("Microsoft YaHei", 12F);
+            this.btnSalir.Location = new System.Drawing.Point(399, 650);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(110, 40);
+            this.btnSalir.TabIndex = 6;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // pbClases
+            // 
+            this.pbClases.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pbClases.Image = global::GenteFitApp.Properties.Resources.mnuClClases;
+            this.pbClases.Location = new System.Drawing.Point(399, 32);
+            this.pbClases.Name = "pbClases";
+            this.pbClases.Size = new System.Drawing.Size(483, 288);
+            this.pbClases.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbClases.TabIndex = 7;
+            this.pbClases.TabStop = false;
+            this.pbClases.Click += new System.EventHandler(this.pbClases_Click);
+            // 
+            // pbMisClases
+            // 
+            this.pbMisClases.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pbMisClases.Image = global::GenteFitApp.Properties.Resources.mnuClMiClases;
+            this.pbMisClases.Location = new System.Drawing.Point(399, 338);
+            this.pbMisClases.Name = "pbMisClases";
+            this.pbMisClases.Size = new System.Drawing.Size(483, 288);
+            this.pbMisClases.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbMisClases.TabIndex = 8;
+            this.pbMisClases.TabStop = false;
+            this.pbMisClases.Click += new System.EventHandler(this.pbMisClases_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::GenteFitApp.Properties.Resources.GenteFit_Entrena_y_vive;
+            this.pictureBox3.Location = new System.Drawing.Point(807, 640);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(75, 62);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 9;
+            this.pictureBox3.TabStop = false;
+            // 
+            // frmMenuCliente
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1267, 714);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pbMisClases);
+            this.Controls.Add(this.pbClases);
+            this.Controls.Add(this.btnSalir);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmMenuCliente";
+            this.Text = "frmMenuCliente";
+            ((System.ComponentModel.ISupportInitialize)(this.pbClases)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbMisClases)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.PictureBox pbClases;
+        private System.Windows.Forms.PictureBox pbMisClases;
+        private System.Windows.Forms.PictureBox pictureBox3;
+    }
+}
